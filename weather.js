f72c9d1b554bade106a27761d2e43e156ed7ee1e
Elaine Mary Rose <iit2016130@iiita.ac.in>
2017-03-31 19:46:28 +0530

    Update weather.js
    Updated the font and display of the home page

--- weather.js
+++ weather.js
@@ -33,15 +33,15 @@ $(document).ready(function(){
 
 function show(data){
 
-	return "<h3 style = 'font-size:38px; text-align:center; font-weight: bold; font-family:'Cinzel';'> CURRENT WEATHER FOR" + " " + data.name + ", " + data.sys.country + "</h3>" +
-		   "<h3 style='padding-left:400px; margin-top:30px;'><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" + 
-		   "<h3 style='padding-left:400px;'><strong>Description</strong>: <img src =  'http://openweathermap.org/img/w/" + data.weather[0].icon+ ".png'>" +  data.weather[0].description + "</h3>" +
-		   "<h3 style='padding-left:400px;'><strong>Current Temperature</strong>: "+ data.main.temp + "&deg;C</h3>" + 
-		   "<h3 style='padding-left:400px;'><strong>Pressure</strong>: "+ data.main.pressure + "hPa</h3>" + 
-		   "<h3 style='padding-left:400px;'><strong>Humidity</strong>: "+ data.main.humidity +"%</h3>" + 
-		   "<h3 style='padding-left:400px;'><strong>Visibility</strong>: "+ data.visibility+"m</h3>" + 
-		   "<h3 style='padding-left:400px;'><strong>Wind Speed</strong>: "+ data.wind.speed +"m/s</h3>" + 
-		   "<h3 style='padding-left:400px;'><strong>Wind Direction</strong>: "+ data.wind.deg +"</h3>" ;
+	return "<h3 style = 'font-family: Cinzel; font-size:34px; text-align:center; font-weight: bold; margin-top:-10px; padding-top: 10px;'> CURRENT WEATHER FOR" + " " + data.name + ", " + data.sys.country + "</h3>" +
+		   "<h3 style='font-family: Cinzel; padding-left:500px; margin-top:30px;'><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" + 
+		   "<h3 style='font-family: Cinzel; padding-left:500px;'><strong>Description</strong>: <img src =  'http://openweathermap.org/img/w/" + data.weather[0].icon+ ".png'>" +  data.weather[0].description + "</h3>" +
+		   "<h3 style='font-family: Cinzel; padding-left:500px;'><strong>Current Temperature</strong>: "+ data.main.temp + "&deg;C</h3>" + 
+		   "<h3 style='font-family: Cinzel; padding-left:500px;'><strong>Pressure</strong>: "+ data.main.pressure + "hPa</h3>" + 
+		   "<h3 style='font-family: Cinzel; padding-left:500px;'><strong>Humidity</strong>: "+ data.main.humidity +"%</h3>" + 
+		   "<h3 style='font-family: Cinzel; padding-left:500px;'><strong>Visibility</strong>: "+ data.visibility+"m</h3>" + 
+		   "<h3 style='font-family: Cinzel; padding-left:500px;'><strong>Wind Speed</strong>: "+ data.wind.speed +"m/s</h3>" + 
+		   "<h3 style='font-family: Cinzel; padding-left:500px; padding-bottom: 10px; margin-bottom: -5px;'><strong>Wind Direction</strong>: "+ data.wind.deg +"<br></h3>" ;
 
 };
 
