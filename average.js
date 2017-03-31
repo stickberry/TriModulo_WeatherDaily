a2a174f19a31f95176a25415610e36b3f3d2ef3d
Elaine Mary Rose <iit2016130@iiita.ac.in>
2017-03-31 19:19:16 +0530

    Created average.js
    Javascript file for the calculation and display of the average of the temperature at 4 different cities.

---
+++ average.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	var avg=0;
+	$('#button2').click(function(){
+
+		var c1 = $("#in1").val();
+		var c2 = $("#in2").val();
+		var c3 = $("#in3").val();
+		var c4 = $("#in4").val();
+		var avg = 0;
+
+		var urls = ['http://api.openweathermap.org/data/2.5/weather?q='+ c1 + '&units=metric' + '&APPID=385d17c855f75959504cef74ca36d244',
+		'http://api.openweathermap.org/data/2.5/weather?q='+ c2 + '&units=metric' + '&APPID=385d17c855f75959504cef74ca36d244',
+		'http://api.openweathermap.org/data/2.5/weather?q='+ c3 + '&units=metric' + '&APPID=385d17c855f75959504cef74ca36d244',
+		'http://api.openweathermap.org/data/2.5/weather?q='+ c4 + '&units=metric' + '&APPID=385d17c855f75959504cef74ca36d244'];
+
+		$.each(urls, function(i,u){ 
+     		$.ajax(u, 
+       		{ 
+       			type: 'GET',
+         		dataType: "jsonp",
+         		success: function (data){
+             		avg = avg + data.main.temp;
+         		} 
+       		});
+     	});
+
+		setTimeout(function(){
+			avg = avg/4.0;
+			var widget = show(avg);
+			$("#show2").html(widget);
+			avg = 0;
+			$("#in1").val('');
+			$("#in2").val('');
+			$("#in3").val('');
+			$("#in4").val('');
+		},3500);
+
+     });
+});
+
+
+function show(avg){
+	return 	"<h3><strong><br />Average Temperature</strong>: "+ avg +"&deg;C</h3>";
+}
